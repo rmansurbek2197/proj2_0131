18d5ac781d084eb5ade94d2f38a5cf40c8258669
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-01-31 12:59:36 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# 6
+other_letters = list(input("Yana harflar kiriting: "))
+letters.extend(other_letters)
+
+# 7
+odd = [1, 3, 5]
+even = [2, 4, 6]
+merged = odd + even
+
+# 8
+numbers.insert(0, int(input("Boshiga qo'shiladigan son: ")))
+
+# 9
+letters.insert(2, "X")
+
+# 10
+odd.insert(1, 0)
+
+# 11
+element = input("Qidirilayotgan element: ")
+print(letters.index(element))
